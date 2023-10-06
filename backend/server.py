a83a4e7f1c5870d3cee9fd0f6d0225bf11fbd004
Arpit Mathur <arpitmathur23@gmail.com>
2023-10-06 23:33:08 +0530

    caching tests

--- backend/server.py
+++ backend/server.py
@@ -6,11 +6,12 @@ from flask_cors import CORS
 import numpy as np
 import time
 
+
 app = Flask(__name__)
 CORS(app)
 
 # Allow requests only from localhost:3000
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:8501"}})
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 # Get SMTP server settings from environment variables
 smtp_server = os.environ.get("smtp_server")  
@@ -19,23 +20,6 @@ smtp_username = os.environ.get("smtp_username")
 smtp_password = os.environ.get("smtp_password")
 sender_email = os.environ.get("sender_email")
 receiver_email = os.environ.get("receiver_email")
-
-
-@app.route('/analysis', methods=['GET'])
-def stream_data():
-
-    def generate_data():
-        folder_path = "./3W/0"
-        for file_name in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, file_name)
-            if os.path.isfile(file_path):
-                data = pd.read_csv(file_path)
-                data_list = list(data['P-TPT'].head())
-                data_string = "  ".join(map(str, data_list))
-                yield f"data: {data_string}\n\n"
-                time.sleep(1)
-
-    return Response(generate_data(), content_type='text/event-stream')
 
 
 def send_email(subject, message):
@@ -54,6 +38,35 @@ def send_email(subject, message):
         return str(e)
 
 
+@app.route('/analysis', methods=['GET'])
+def stream_data():
+    folder_path = "./3W/0"
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        if os.path.isfile(file_path):
+            data = pd.read_csv(file_path)
+            data_list = list(data['P-TPT'].head(700))
+            data_string = "  ".join(map(str, data_list))
+            return jsonify(data_string)
+
+@app.route('/test', methods=['GET'])
+def fun():
+    with open('./cache.txt','w+') as f:
+        counter=f.read()
+        folder_path = "./3W/0"
+        for file_name in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file_name)
+            if os.path.isfile(file_path):
+                data = pd.read_csv(file_path)
+                data_list = list(data['P-TPT'])
+                data_list=data_list[counter:counter+700]
+                counter+=700
+                f.write(counter)
+                data_string = "  ".join(map(str, data_list))
+                return jsonify(data_string)
+
+
+
 @app.route('/send_email',methods=['POST'])
 def send_email():
 
@@ -61,7 +74,7 @@ def send_email():
     data=request.get_json()
     data=np.array(data)
 
-    # model=load('./model.keras)
+    
 
     # output=model.predict(data.reshape(1,-1))
 
