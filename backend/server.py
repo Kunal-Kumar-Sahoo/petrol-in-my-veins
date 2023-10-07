0ab186b50674a0403514289c68842b11470adce1
Kunal Kumar Sahoo <kunal.sahoo2003@gmail.com>
2023-10-07 06:46:03 +0530

    recovery

--- backend/server.py
+++ backend/server.py
@@ -38,16 +38,16 @@ prediction_labels = [
     "Hydrate in production line"
 ]
 
-folder_path = "./3W/0"
-data_list0, data_list1, data_list2, data_list3 , timestamp_list = [], [], [], [],[]
-def load_data(file_path):
-    if os.path.isfile(file_path):
-        data = pd.read_csv(file_path)
-        timestamp_list.extend(list(data['timestamp']))
-        data_list0.extend(list(data['P-PDG']))
-        data_list1.extend(list(data['P-TPT']))
-        data_list2.extend(list(data['T-TPT']))
-        data_list3.extend(list(data['P-MON-CKP']))
+# folder_path = "./3W/0"
+# data_list0, data_list1, data_list2, data_list3 , timestamp_list = [], [], [], [],[]
+# def load_data(file_path):
+#     if os.path.isfile(file_path):
+#         data = pd.read_csv(file_path)
+#         timestamp_list.extend(list(data['timestamp']))
+#         data_list0.extend(list(data['P-PDG']))
+#         data_list1.extend(list(data['P-TPT']))
+#         data_list2.extend(list(data['T-TPT']))
+#         data_list3.extend(list(data['P-MON-CKP']))
     
 def send_email(subject, message):
     try:
@@ -64,9 +64,9 @@ def send_email(subject, message):
     except Exception as e:
         return str(e)
 
-for file_name in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file_name)
-        load_data(file_path)
+# for file_name in os.listdir(folder_path):
+#         file_path = os.path.join(folder_path, file_name)
+#         load_data(file_path)
 
 @app.route('/analysis', methods=['GET'])
 def get_next():
@@ -108,45 +108,44 @@ def get_next():
 
 
 
-@app.route('/send_email',methods=['POST'])
+@app.route('/send_email',methods=['GET'])
 def send_email():
 
-    message = request.get_json()
-    data=request.get_json()
-    data=np.array(data)
+   output=send_preds()
+   return output
 
     # output=model.predict(data.reshape(1,-1))
 
-    output = [0]
+    # output = [0]
 
-    # Determine the prediction labels and create the report table
-    report_table = []
-    for entry in output:
-        if 0 <= entry[0] < len(prediction_labels):
-            prediction_label = prediction_labels[int(entry[0])]
-        else:
-            prediction_label = "Unknown"
+    # # Determine the prediction labels and create the report table
+    # report_table = []
+    # for entry in output:
+    #     if 0 <= entry[0] < len(prediction_labels):
+    #         prediction_label = prediction_labels[int(entry[0])]
+    #     else:
+    #         prediction_label = "Unknown"
         
-        prediction_value = entry[1]
-        report_table.append([f'Sucker Rod Pump ID :{entry}',prediction_label, prediction_value])
+    #     prediction_value = entry[1]
+    #     report_table.append([f'Sucker Rod Pump ID :{entry}',prediction_label, prediction_value])
 
-    # Send an email with the report table
-    subject = "Fault Detection Prediction Report"
-    message = "Prediction Results:\n\n"
-    message += "Prediction Label\tPrediction Value\n"
-    for entry in report_table:
-        message += f"\t{entry[1]}\t{entry[2]}\n"
+    # # Send an email with the report table
+    # subject = "Fault Detection Prediction Report"
+    # message = "Prediction Results:\n\n"
+    # message += "Prediction Label\tPrediction Value\n"
+    # for entry in report_table:
+    #     message += f"\t{entry[1]}\t{entry[2]}\n"
 
-    # Replace with your email sending function
-    send_email(subject, message)
+    # # Replace with your email sending function
+    # send_email(subject, message)
 
-    # Return the report table as JSON (optional)
-    return jsonify(report_table)
+    # # Return the report table as JSON (optional)
+    # return jsonify(report_table)
 
 
 def load_dataset():
     dataset=[]
-    with open(r'data/2.csv', newline='') as f:
+    with open(r'./2.csv', newline='') as f:
         reader = csv.reader(f)
         for row in reader:
             dataset.append(row)
@@ -167,12 +166,31 @@ def dataset_creator():
 
 
 def send_preds():
-    model = keras.models.load('backend/iteration1.keras')
-    p1, p2, ts2, sc = dataset_creator()
-    response_content = predict_(model, [p1, p2, ts2, sc])
-    for i in range(5):
-        response_content[i] = [prediction_labels[i] for i in response_content[i]]
-    return response_content
+    with open('./response_cache.txt','r+') as f:
+        counter=int(f.read())
+        endcounter=int(int(counter)+5)
+
+        model = keras.models.load_model('./iteration1.keras')
+        p1, p2, ts2, sc = dataset_creator()
+
+        response_content = predict_(model, [p1, p2, ts2, sc])
+        updated_response=[]
+
+        for i in range(counter,endcounter):
+            j=response_content[0][i]
+            response_content[0][i] = prediction_labels[j]
+            print(response_content[0][i])
+
+        try:
+            for i in range(counter,endcounter):
+                updated_response.append([str(response_content[0][i]), str(max(response_content[1][i])*100)])
+        except Exception as e:
+            print("Exception: ",e)
+
+        f.seek(0)
+        f.write(str(endcounter))
+        print(updated_response)
+        return updated_response
 
 if __name__ == '__main__':
     app.run(debug=True)
